3491a6c02b87e3a2570a4f2a80413e619d895ebb
aa.parky <aa.parky@gmail.com>
2023-01-06 06:26:22 +0000

    spelling stuffs

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(custom-safe-themes
    '("adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "443e2c3c4dd44510f0ea8247b438e834188dc1c6fb80785d83ad3628eadf9294" default))
  '(package-selected-packages
-   '(undo-fu evil ace-jump-mode counsel-osx-app counsel ivy ace-window which-key exec-path-from-shell doom-modeline doom-themes toml-mode company flycheck lsp-ui lsp-mode rustic multi-vterm vterm magit use-package)))
+   '(flycheck undo-fu evil ace-jump-mode counsel-osx-app counsel ivy ace-window which-key exec-path-from-shell doom-modeline doom-themes toml-mode company lsp-ui lsp-mode rustic multi-vterm vterm magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
